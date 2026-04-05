8b47936ab4209f0fa722ea9431127a6c15e6fe29
YUNA9627 <yuna2175@naver.com>
2026-04-05 23:55:24 +0900

    feat: 연락처 검색 기능 추가

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBox from "./SearchBox";
 import ContactItem from "./ContactItem";
 import { useSelector } from "react-redux";
 
 const ContactList = () => {
-  const contactList = useSelector(state => state.contactList);
+  const contactList = useSelector((state) => state.contactList);
+  const [inputValue, setInputValue] = useState("");
+  const [search, setSearch] = useState("");
+
+  const filteredList = contactList.filter((item) => {
+    const keyword = search.toLowerCase();
+
+    return (
+      item.name.toLowerCase().includes(keyword) ||
+      item.phoneNumber.includes(search)
+    );
+  });
+
+  const handleSearch = () => {
+    setSearch(inputValue);
+  };
+
   return (
-    <div>
-      <SearchBox />
-      {contactList.map((item) => (
-        <ContactItem item={item} />
-      ))}
+    <div className="contact-list-wrap">
+      <SearchBox
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        handleSearch={handleSearch}
+      />
+
+      <p className="section-title list-title">연락처 목록</p>
+
+      <div className="contact-items-card">
+        {filteredList.map((item, index) => (
+          <ContactItem key={index} item={item} index={index} />
+        ))}
+      </div>
     </div>
   );
 };
