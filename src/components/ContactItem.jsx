fc4eafd24884800a17665ffecba0008f8403d23d
YUNA9627 <yuna2175@naver.com>
2026-04-05 23:58:36 +0900

    refactor: ContactItem 프로필 UI 구조 변경

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import { Row, Col } from "react-bootstrap";
+import React from "react";
+import "../ContactForm.css";
 
-const ContactItem = ({item}) => {
+const ContactItem = ({ item, index }) => {
+  const profileText = item.name ? item.name.slice(0, 2) : "";
+
+  const profileColors = [
+    "#1e90ff",
+    "#22c55e",
+    "#f59e0b",
+    "#a855f7",
+    "#ef4444",
+    "#14b8a6",
+  ];
+
+  const profileColor = profileColors[index % profileColors.length];
+
   return (
-    <div>
-      <Row>
-        <Col lg={1}>
-          <img width={50} src="https://i.namu.wiki/i/M0j6sykCciGaZJ8yW0CMumUigNAFS8Z-dJA9h_GKYSmqqYSQyqJq8D8xSg3qAz2htlsPQfyHZZMmAbPV-Ml9UA.webp" alt="" />
-        </Col>
-        <Col lg={11}>
-          <div>{item.name}</div>
-          <div>{item.phoneNumber}</div>
-        </Col>
-      </Row>
-    </div>
-  )
-}
+    <div className="contact-item">
+      <div className="contact-item-left">
+        <div
+          className="contact-profile"
+          style={{ backgroundColor: profileColor }}
+        >
+          {profileText}
+        </div>
 
-export default ContactItem
+        <div className="contact-info">
+          <div className="contact-name">{item.name}</div>
+          <div className="contact-phone">{item.phoneNumber}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactItem;
